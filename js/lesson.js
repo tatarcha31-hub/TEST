1408006d38dac610b8bb07f5e4e5ae3b7aa8d956
Ildar <bogdanovildar31@gmail.com>
2026-06-11 11:45:40 +0600

    first commit

--- js/lesson.js
+++ js/lesson.js
@@ -41,3 +41,118 @@ setInterval(() => {
     }
     selectTab(curuentIndex);
 }, 3000);
+
+const somInput = document.querySelector('#som');
+const usdInput = document.querySelector('#usd');
+const eurInput = document.querySelector('#eur');
+const errorText = document.querySelector('#error');
+const cnyInput = document.querySelector('#cny');
+const rubInput = document.querySelector('#rub');
+
+const getRates = () => {
+    const request = new XMLHttpRequest();
+    request.open('GET', '/data/converter.json');
+    request.send();
+
+    request.onload = () => {
+        if (request.status >= 200 && request.status < 300) {
+            const data = JSON.parse(request.response);
+            const usdRate = data.usd;
+            const eurRate = data.euro;
+            const rubRate = data.rub;
+            const cnyRate = data.cny;
+
+            somInput.oninput = () => {
+                if (somInput.value === '') {
+                    usdInput.value = '';
+                    eurInput.value = '';
+                    cnyInput.value = '';
+                    rubInput.value = '';
+                    return;
+                }
+
+                usdInput.value = (somInput.value / usdRate).toFixed(2);
+                eurInput.value = (somInput.value / eurRate).toFixed(2);
+                cnyInput.value = (somInput.value / cnyRate).toFixed(2);
+                rubInput.value = (somInput.value / rubRate).toFixed(2);
+            };
+
+            usdInput.oninput = () => {
+                if (usdInput.value === '') {
+                    somInput.value = '';
+                    eurInput.value = '';
+                    cnyInput.value = '';
+                    rubInput.value = '';
+                    return;
+                }
+
+                const som = usdInput.value * usdRate;
+                somInput.value = som.toFixed(2);
+                eurInput.value = (som / eurRate).toFixed(2);
+                rubInput.value = (som / rubRate).toFixed(2);
+                cnyInput.value = (som / cnyInput).toFixed(2);
+            };
+
+            eurInput.oninput = () => {
+                if (eurInput.value === '') {
+                    somInput.value = '';
+                    usdInput.value = '';
+                    cnyInput.value = '';
+                    rubInput.value = '';
+                    return;
+                }
+
+                const som = eurInput.value * eurRate;
+                somInput.value = som.toFixed(2);
+                usdInput.value = (som / usdRate).toFixed(2);
+                rubInput.value = (som / rubRate).toFixed(2);
+                cnyInput.value = (som / cnyRate).toFixed(2);
+            };
+
+            rubInput.oninput = () => {
+                if (rubInput,value = ''){
+                    somInput.value = '';
+                    usdInput.value = '';
+                    eurInput.value = '';
+                    rubInput.value = '';
+                    cnyInput.value = '';
+                    return;
+                }
+
+                const som = rubInput.value * rubRate;
+                somInput.value = som.toFixed(2);
+                usdInput.value = (som / usdRate).toFixed(2);
+                eurInput.value = (som / eurRate).toFixed(2);
+                cnyInput.value = (som / cnyRate).toFixed(2);
+            };
+            
+            cnyInput.oninput = () => {
+                if (rubInput,value = ''){
+                    somInput.value = '';
+                    usdInput.value = '';
+                    eurInput.value = '';
+                    rubInput.value = '';
+                    cnyInput.value = '';
+                    return;
+                }
+
+                const som = cnyInput.value * cnyRate;
+                somInput.value = som.toFixed(2);
+                usdInput.value = (som / usdRate).toFixed(2);
+                eurInput.value = (som / eurRate).toFixed(2);
+                rubInput.value = (som / rubRate).toFixed(2);
+            };
+
+        } else {
+            errorText.style.color = 'red';
+            errorText.textContent = 'Произошла ошибка на сервере';
+        }
+    };
+
+    request.onerror = () => {
+        errorText.style.color = 'red';
+        errorText.textContent = 'Ошибка соединения';
+    };
+};
+
+getRates();
